3d8534384cf7d90ee7daaa52be8c5ea87cd7d339
LoopingDiego <60360505+LoopingDiego@users.noreply.github.com>
2020-09-02 17:29:02 -0500

    [add]: jinja2 render template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+""" Flask web application
+"""
+
+from flask import Flask, render_template
+app = Flask(__name__, template_folder='templates')
+
+
+@app.route('/', strict_slashes=False)
+def greetings():
+    """ Hello function
+    """
+
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ hbnb direction
+    """
+
+    return 'HBNB'
+
+
+@app.route('/c/<txt>', strict_slashes=False)
+def c_route(txt):
+    """ String with C as first char
+    """
+
+    return "C {}".format(txt.replace('_', ' '))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<txt>', strict_slashes=False)
+def python_route(txt='is cool'):
+    """ String with Python as first word
+    """
+
+    return "Python {}".format(txt.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_route(n):
+    """ number testing
+    """
+
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def num_template(n):
+    """ Return a html page with n H1 tag
+    """
+
+    return render_template('5-number.html',
+                           title="Number: {}".format(n))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
