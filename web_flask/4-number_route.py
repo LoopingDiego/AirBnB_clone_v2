d470c9a7b3fd72cf06bda124df8524dd2105d80a
LoopingDiego <60360505+LoopingDiego@users.noreply.github.com>
2020-09-02 16:48:47 -0500

    [add]: int testing with app route

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+""" Flask web application
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def greetings():
+    """ Hello function
+    """
+
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ hbnb direction
+    """
+
+    return 'HBNB'
+
+
+@app.route('/c/<txt>', strict_slashes=False)
+def c_route(txt):
+    """ String with C as first char
+    """
+
+    return "C {}".format(txt.replace('_', ' '))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<txt>', strict_slashes=False)
+def python_route(txt='is cool'):
+    """ String with C as first char
+    """
+
+    return "Python {}".format(txt.replace('_', ' '))
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def number_route(n):
+    if int(n):
+        return "{} is a number".format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
